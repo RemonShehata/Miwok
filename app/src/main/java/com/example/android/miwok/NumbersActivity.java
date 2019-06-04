f96b3842dd04a835dd122ae1a8140bdde5385dcb
Remon <remongerges94@gmail.com>
2019-06-04 16:49:54 +0200

    Add TextViews to the layout from Java code

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,8 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -23,5 +25,18 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("Eight");
         words.add("Nine");
         words.add("Ten");
+
+        LinearLayout rootView = findViewById(R.id.root_view);
+        TextView wordView = new TextView(this);
+        wordView.setText(words.get(0));
+        rootView.addView(wordView);
+
+        TextView wordView1 = new TextView(this);
+        wordView1.setText(words.get(1));
+        rootView.addView(wordView1);
+
+        TextView wordView2 = new TextView(this);
+        wordView2.setText(words.get(2));
+        rootView.addView(wordView2);
     }
 }
